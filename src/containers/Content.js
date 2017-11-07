d8f441ed64dfc80650702edc3865d2c09742e048
Aakash Sheth <avsheth@iastate.edu>
2017-11-06 18:30:00 -0600

    Removed code. Back to working state

--- src/containers/Content.js
+++ src/containers/Content.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Grid from '../components/Grid'
 
 export default class Content extends React.Component {      
     render() {
         return (
             <div className="content" id="content">
                 This is the main content.
-                <Grid />
+                
             </div>
         );
     }
